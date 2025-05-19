3d3a5b96282c2ee46d14e908817ac05c694c192f
José Gonzales Montaño <85416653+TillWurner@users.noreply.github.com>
2025-05-19 19:36:15 -0400

    Add files via upload

---
+++ Restaurante/MenuItem.java
@@ -0,0 +1,21 @@
+package Restaurante;
+
+public class MenuItem extends MenuComponent {
+    private String name;
+    private double price;
+
+    public MenuItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Elemento: " + name + ", Precio: " + price);
+    }
+}
